aaf6e5f7b009a8a47e6a012ae08344020da0652f
toleberdyyev <fredcolin079@gmail.com>
2016-07-15 11:30:39 +0600

    06 save

--- blog/static/js/ajax.js
+++ blog/static/js/ajax.js
@@ -1,7 +1,8 @@
+var captcha = $("#gogl_captcha").html()
+console.log(captcha.length)
 $(document).ready(function(){
   $('#comment').submit(function(){
     var str=$(this).serialize()
-    console.log(str);
     var content = $('#comments_content').html()
     var author = $('#id_author').val()
     var text =$('#id_text').val()
@@ -11,12 +12,11 @@ $(document).ready(function(){
       data:str,
       success:function(html){
         $('#comments_content').html(content+data_l);
-
-
+        console.log(captcha.length)
+        $('#id_author').val('')
+        $('#id_text').val('')
       }
-
-
-    })
+})
     return false;
 
   })
